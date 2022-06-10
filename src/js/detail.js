9b7b3d914dc9cf8d036f41179441b2e03617c39d
tuanpaph20520 <tuanpaph20520@fpt.edu.vn>
2022-06-10 19:58:43 +0700

    update codes

--- src/js/detail.js
+++ src/js/detail.js
@@ -1,4 +1,5 @@
-const productList=[
+//products in detail
+const productsDetail = [
   {
     id: 1,
     name: "Big and Juicy Wagyu Beef Cheeseburger",
@@ -41,41 +42,82 @@ img: "https://res.cloudinary.com/dn3jydehx/image/upload/v1654695402/images-Lab3/
 time: 30,
 type: "Sweet",
 },
-];
+  ];
+  
+  function showProductsDetail(products) {
+    if (!Array.isArray(products) || products.length == 0) return false;
+  
+    let result = "";
+    for (let i = 0; i < 3; i++) {
+        const product = products[i];
+        result +=`<div
+        class="shadow hover:shadow-xl group duration-500 rounded-t-3xl hover:bg-gray-50 rounded-b-md"
+      >
+        <div>
+          <a href="./detail.html?id=${product.id}"
+            ><img
+              src="${product.img}"
+              class="rounded-3xl w-full"
+              alt=""
+          /></a>
+        </div>
+        <div>
+          <h3
+            class="ml-2 text-lg text-[#111928] group-hover:text-red-500 font-semibold mt-4"
+          >
+            <a href="./detail.html?id=${product.id}">${product.name}</a>
+          </h3>
+        </div>
+        <p class="ml-2 text-xl font-semibold my-1 text-red-600">$30</p>
+        <button
+          class="text-sm font-semibold my-2 py-3 text-white rounded-md w-full bg-gray-800 hover:text-red-500 hover:bg-gray-900"
+        >
+          Add To Cart
+        </button>
+      </div>`;
+    }
+    return result;
+  }
+  
+  document.getElementById("products").innerHTML = showProductsDetail(productsDetail);
 
-//Lấy ID trên url
-const id = new URLSearchParams(window.location.search).get("id");
 
-function showProduct(products){
+  //show product
+  
+  //Lấy ID trên url
+  const id = new URLSearchParams(window.location.search).get("id");
+  
+  function showProduct(products){
+      //Nếu không phải 1 mảng hoặc không có giá trị trong mảng thì return false
+      if(!Array.isArray(products) || products.length ==0) return false;
+      let result = "";
+  
+      //Tìm phần tử trong mảng, nếu id trên url == id phần tử trong mảng
+      //thì trả về object đấy
+      const product = products.find((item) => item.id ==id);
+      
+      //Trả về kết quả là một chuỗi
+      result +=`
+      <h2 class="font-black text-4xl">
+       ${product.name}
+      </h2>`;
+      return result;
+  };
+  function showImage(products){
     //Nếu không phải 1 mảng hoặc không có giá trị trong mảng thì return false
     if(!Array.isArray(products) || products.length ==0) return false;
     let result = "";
-
+  
     //Tìm phần tử trong mảng, nếu id trên url == id phần tử trong mảng
     //thì trả về object đấy
     const product = products.find((item) => item.id ==id);
     
     //Trả về kết quả là một chuỗi
     result +=`
-    <h2 class="font-black text-4xl">
-     ${product.name}
-    </h2>`;
+    <img class="w-full" src="${product.img}" alt="" />`;
     return result;
-};
-function showImage(products){
-  //Nếu không phải 1 mảng hoặc không có giá trị trong mảng thì return false
-  if(!Array.isArray(products) || products.length ==0) return false;
-  let result = "";
-
-  //Tìm phần tử trong mảng, nếu id trên url == id phần tử trong mảng
-  //thì trả về object đấy
-  const product = products.find((item) => item.id ==id);
+  };
   
-  //Trả về kết quả là một chuỗi
-  result +=`
-  <img class="w-full" src="${product.img}" alt="" />`;
-  return result;
-};
-
-document.getElementById("detail").innerHTML = showProduct(productList);
-document.getElementById("image").innerHTML = showImage(productList);
+  document.getElementById("detail").innerHTML = showProduct(productsDetail);
+  document.getElementById("image").innerHTML = showImage(productsDetail);
+  
